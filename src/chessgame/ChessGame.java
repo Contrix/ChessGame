0210a08633a4e75fb0686390179196cc9b4c1da3
Contrix <contrix.hanaji@gmail.com>
2014-12-19 19:03:06 +0100

    Vytvoření projektu

---
+++ src/chessgame/ChessGame.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chessgame;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Jirka
+ */
+public class ChessGame extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        /*Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        */
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 600, 600);
+        primaryStage.setTitle("Chess game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
